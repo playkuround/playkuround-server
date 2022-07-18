3bfaaeea9ef61a414af00b9b1d877bf3ded7cae7
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-07-18 15:28:30 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/playkuround/playkuroundserver/PlaykuroundServerApplication.java
@@ -0,0 +1,13 @@
+package com.playkuround.playkuroundserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaykuroundServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaykuroundServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playkuround/playkuroundserver/PlaykuroundServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playkuround.playkuroundserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaykuroundServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

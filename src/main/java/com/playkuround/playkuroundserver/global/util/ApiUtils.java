08d6a65c3b84cfb6b39b99884fd9a39f71557f70
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-08-05 02:12:05 +0900

    refactor: 에러 응답 폼 수정

--- src/main/java/com/playkuround/playkuroundserver/global/util/ApiUtils.java
+++ src/main/java/com/playkuround/playkuroundserver/global/util/ApiUtils.java
@@ -2,17 +2,21 @@ package com.playkuround.playkuroundserver.global.util;
 
 import com.playkuround.playkuroundserver.global.common.response.ApiResult;
 import com.playkuround.playkuroundserver.global.error.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class ApiUtils {
 
     private ApiUtils() {}
 
     public static <T> ApiResult<T> success(T response) {
+        new ApiResult<>(true, response, null);
         return new ApiResult<>(true, response, null);
     }
 
-    public static ApiResult<ErrorResponse> error(ErrorResponse errorResponse) {
-        return new ApiResult<>(false, null, errorResponse);
+    public static ResponseEntity<ApiResult<ErrorResponse>> error(ErrorResponse errorResponse) {
+        ApiResult<ErrorResponse> apiResult = new ApiResult<>(false, null, errorResponse);
+        return new ResponseEntity<>(apiResult, HttpStatus.valueOf(apiResult.getErrorResponse().getStatus()));
     }
 
 }

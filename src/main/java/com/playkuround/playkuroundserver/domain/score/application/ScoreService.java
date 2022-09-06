3087a839348c647e5b6da7fc32b193e70e316b4e
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-09-07 03:09:07 +0900

    refactor: 점수 등록 요청 DTO 자료형 변경

--- src/main/java/com/playkuround/playkuroundserver/domain/score/application/ScoreService.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/score/application/ScoreService.java
@@ -1,7 +1,7 @@
 package com.playkuround.playkuroundserver.domain.score.application;
 
 import com.playkuround.playkuroundserver.domain.score.dao.ScoreRepository;
-import com.playkuround.playkuroundserver.domain.score.dto.SaveScore;
+import com.playkuround.playkuroundserver.domain.score.dto.ScoreRegisterDto;
 import com.playkuround.playkuroundserver.domain.score.dto.ScoreRankingDto;
 import com.playkuround.playkuroundserver.domain.score.exception.ScoreNotFoundException;
 import com.playkuround.playkuroundserver.domain.user.dao.UserFindDao;
@@ -23,7 +23,7 @@ public class ScoreService {
     private final UserFindDao userFindDao;
 
     @Transactional
-    public void saveScore(String userEmail, SaveScore saveScore) {
+    public void saveScore(String userEmail, ScoreRegisterDto saveScore) {
         // TODO 검증의 범위는 어디까지? 예를 들면, 진짜로 유저가 탐험을 했는지 안했는지 확인까지 해야하는가?
         User user = userFindDao.findByEmail(userEmail);
         scoreRepository.save(saveScore.toEntity(user));

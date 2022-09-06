e3c36c5e599cf7d7f424acf38b94f76fd9806803
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-09-07 03:07:32 +0900

    feat: 학과 Enum 추가

--- src/main/java/com/playkuround/playkuroundserver/global/validation/ValidEnum.java
+++ src/main/java/com/playkuround/playkuroundserver/global/validation/ValidEnum.java
@@ -11,11 +11,13 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
-    String message() default "Invalid value!";
+
+    String message() default "Enum 에 없는 값입니다.";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
 
     Class<? extends java.lang.Enum<?>> enumClass();
+
 }

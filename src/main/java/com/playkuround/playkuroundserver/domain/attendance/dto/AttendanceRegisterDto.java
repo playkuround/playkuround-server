dd57801990fc04af0c3573f3859a07485750af84
seungtaekhong <hsk4991149@naver.com>
2023-01-08 22:08:22 +0900

    test: attendance api 테스트 추가

--- src/main/java/com/playkuround/playkuroundserver/domain/attendance/dto/AttendanceRegisterDto.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/attendance/dto/AttendanceRegisterDto.java
@@ -1,9 +1,12 @@
 package com.playkuround.playkuroundserver.domain.attendance.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.playkuround.playkuroundserver.domain.badge.domain.BadgeType;
 import com.playkuround.playkuroundserver.global.validation.Latitude;
 import com.playkuround.playkuroundserver.global.validation.Longitude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ public class AttendanceRegisterDto {
 
     @Getter
     @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Request {
         @Latitude
         private Double latitude;
@@ -23,18 +28,20 @@ public class AttendanceRegisterDto {
 
     @Getter
     @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Response {
 
         private List<BadgeInfo> newBadges = new ArrayList<>();
 
+        @NoArgsConstructor
+        @AllArgsConstructor
         private static class BadgeInfo {
+            @JsonProperty("name")
             String name;
-            String description;
 
-            public BadgeInfo(String name, String description) {
-                this.name = name;
-                this.description = description;
-            }
+            @JsonProperty("description")
+            String description;
         }
 
         public void addBadge(BadgeType badgeType) {

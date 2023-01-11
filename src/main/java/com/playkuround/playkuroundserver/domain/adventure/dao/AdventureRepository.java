9bd0e5b012949a3577096a46fae42dda2c5e4abd
seungtaekhong <hsk4991149@naver.com>
2023-01-11 19:58:06 +0900

    fix: AdventureRepository jpql 수정(countAdventureForNEIL_ARMSTRONG, or -> and)

--- src/main/java/com/playkuround/playkuroundserver/domain/adventure/dao/AdventureRepository.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/adventure/dao/AdventureRepository.java
@@ -25,7 +25,7 @@ public interface AdventureRepository extends JpaRepository<Adventure, Long> {
     @Query("SELECT COUNT(*) FROM Adventure a where a.landmark.id=37 or a.landmark.id=38")
     Long countAdventureForNATIONAL_PLAYER();
 
-    @Query("SELECT COUNT(*) FROM Adventure a where a.landmark.id>=39 or a.landmark.id<=44")
+    @Query("SELECT COUNT(*) FROM Adventure a where a.landmark.id>=39 and a.landmark.id<=44")
     Long countAdventureForNEIL_ARMSTRONG();
 
     Long countByUser(User user);

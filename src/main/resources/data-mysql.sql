698f00ea3b18d29c0084c8ea73108d6e404c1c05
seungtaekhong <hsk4991149@naver.com>
2023-03-07 21:47:09 +0900

    feat: landmark 초기 데이터 수정

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,93 @@
+-- 랜드마크 초기 데이터입니다. mysql 문법에 맞춰져 있습니다.
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (1, NOW(), NOW(),  37.539765, 127.073215, 40, '산학협동관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (2, NOW(), NOW(),  37.540296, 127.073410, 20, '입학정보관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (3, NOW(), NOW(),  37.539310, 127.074590, 35, '수의학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (4, NOW(), NOW(),  37.540184, 127.074179, 30, '동물생명과학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (5, NOW(), NOW(),  37.540901, 127.074055, 40, '생명과학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (6, NOW(), NOW(),  37.542051, 127.073808, 55, '상허도서관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (7, NOW(), NOW(),  37.541461, 127.072215, 36, '의생명과학연구관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (8, NOW(), NOW(),  37.542908, 127.072815, 50, '예디대', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (9, NOW(), NOW(),  37.542533, 127.074700, 35, '언어교육원', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (10, NOW(), NOW(), 37.541667, 127.075046, 50, '법학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (11, NOW(), NOW(), 37.542375, 127.075711, 30, '상허박물관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (12, NOW(), NOW(), 37.543222, 127.075305, 44, '행정관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (13, NOW(), NOW(), 37.543998, 127.074145, 50, '교육과학관(사범대)', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (14, NOW(), NOW(), 37.544018, 127.075141, 35, '상허연구관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (15, NOW(), NOW(), 37.544319, 127.076184, 40, '경영관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (16, NOW(), NOW(), 37.543374, 127.077314, 40, '새천년관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (17, NOW(), NOW(), 37.543732, 127.078482, 25, '건축관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (18, NOW(), NOW(), 37.543283, 127.078093, 25, '부동산학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (19, NOW(), NOW(), 37.542602, 127.078250, 40, '인문학관(인문대)', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (20, NOW(), NOW(), 37.541954, 127.077748, 30, '학생회관', 'TIMER');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (21, NOW(), NOW(), 37.541298, 127.077828, 35, '제2학생회관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (22, NOW(), NOW(), 37.541655, 127.078769, 33, '공학관 A', 'TIMER');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (23, NOW(), NOW(), 37.542004, 127.079563, 33, '공학관 B', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (24, NOW(), NOW(), 37.541226, 127.079357, 28, '공학관 C', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (25, NOW(), NOW(), 37.540455, 127.079304, 40, '신공학관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (26, NOW(), NOW(), 37.541491, 127.080565, 43, '과학관(이과대)', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (27, NOW(), NOW(), 37.541114, 127.081743, 45, '창의관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (28, NOW(), NOW(), 37.542220, 127.080961, 33, '공예관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (29, NOW(), NOW(), 37.539995, 127.077180, 40, '국제학사', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (30, NOW(), NOW(), 37.539147, 127.078248, 75, '기숙사', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (31, NOW(), NOW(), 37.530744, 127.075539, 44, '일감호', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (32, NOW(), NOW(), 37.541688, 127.077344, 18, '홍예교', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (33, NOW(), NOW(), 37.543135, 127.076172, 20, '황소동상', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (34, NOW(), NOW(), 37.542366, 127.076835, 30, '청심대', 'TIMER');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (35, NOW(), NOW(), 37.541454, 127.082172, 25, '상허박사 동상', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (36, NOW(), NOW(), 37.541475, 127.077802,  0, '노천극장 중앙', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (37, NOW(), NOW(), 37.544387, 127.077593, 65, '대운동장', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (38, NOW(), NOW(), 37.544456, 127.079587, 50, '실내체육관', 'QUIZ');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (39, NOW(), NOW(), 37.545122, 127.076613, 20, '건국문(후문)', 'MOON');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (40, NOW(), NOW(), 37.541827, 127.071780, 15, '중문', 'MOON');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (41, NOW(), NOW(), 37.539165, 127.074042, 15, '일감문(동물병원)', 'MOON');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (42, NOW(), NOW(), 37.539692, 127.072491, 25, '상허문(도서관)', 'MOON');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (43, NOW(), NOW(), 37.541584, 127.082226, 15, '구의역쪽문', 'MOON');
+insert into landmark (id, created_at, updated_at, latitude, longitude, recognition_radius, name, game_type) values (44, NOW(), NOW(), 37.539255, 127.076741, 20, '기숙사쪽문', 'MOON');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

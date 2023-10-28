dcd1deb9ddd53b612f28ff2c59840f681bfce703
seungtaekhong <hsk4991149@naver.com>
2023-10-28 14:31:10 +0900

    refactor: AuthEmailSendService

--- src/main/java/com/playkuround/playkuroundserver/domain/auth/email/dto/AuthEmailSendDto.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/auth/email/dto/AuthEmailSendDto.java
@@ -1,42 +1,32 @@
 package com.playkuround.playkuroundserver.domain.auth.email.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
-
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 public class AuthEmailSendDto {
 
     @Getter
-    @Setter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class Request {
-        @NotBlank
         @Email
         private String target;
     }
 
     @Getter
-    @Setter
-    @Builder
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class Response {
-
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime expireAt;
 
         private Long sendingCount;
-
-        public static Response of(LocalDateTime expireAt, Long sendingCount) {
-            return Response.builder()
-                    .expireAt(expireAt)
-                    .sendingCount(sendingCount)
-                    .build();
-        }
     }
 
 }

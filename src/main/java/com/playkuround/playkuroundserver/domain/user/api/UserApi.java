e0f99c07f0df26b538218cc27befa155fa1123a5
seungtaekhong <hsk4991149@naver.com>
2023-10-29 22:16:33 +0900

    delete: JasyptConfig and JpaConfig

--- src/main/java/com/playkuround/playkuroundserver/domain/user/api/UserApi.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/user/api/UserApi.java
@@ -13,7 +13,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController

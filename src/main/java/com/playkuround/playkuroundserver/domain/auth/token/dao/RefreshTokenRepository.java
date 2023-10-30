d8ef423bbd0905caadf8748259bdf71aaa92c90b
seungtaekhong <hsk4991149@naver.com>
2023-10-30 15:57:25 +0900

    delete: RefreshTokenFindDao 삭제(사용하지 않음)

--- src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/RefreshTokenRepository.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/RefreshTokenRepository.java
@@ -3,11 +3,7 @@ package com.playkuround.playkuroundserver.domain.auth.token.dao;
 import com.playkuround.playkuroundserver.domain.auth.token.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-    Optional<RefreshToken> findByUserEmail(String userEmail);
 
     boolean existsByUserEmail(String userEmail);
 

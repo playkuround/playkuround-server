b921c0952154773ae03cdb85e6174b5ec76c7cf1
seungtaekhong <hsk4991149@naver.com>
2023-10-30 15:59:32 +0900

    refactor: unused import

--- src/main/java/com/playkuround/playkuroundserver/global/config/WebSecurityConfig.java
+++ src/main/java/com/playkuround/playkuroundserver/global/config/WebSecurityConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;

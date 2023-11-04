e439923e1b2a8503d34d2f9fcea0a39b0f172792
seungtaekhong <hsk4991149@naver.com>
2023-11-04 15:02:36 +0900

    refactor: LandmarkFindNearService 코드 리팩토링

--- src/main/java/com/playkuround/playkuroundserver/domain/landmark/application/LandmarkFindNearService.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/landmark/application/LandmarkFindNearService.java
@@ -2,7 +2,7 @@ package com.playkuround.playkuroundserver.domain.landmark.application;
 
 import com.playkuround.playkuroundserver.domain.landmark.dao.LandmarkRepository;
 import com.playkuround.playkuroundserver.domain.landmark.domain.Landmark;
-import com.playkuround.playkuroundserver.domain.landmark.dto.FindNearLandmark;
+import com.playkuround.playkuroundserver.domain.landmark.dto.FindNearLandmarkResponse;
 import com.playkuround.playkuroundserver.global.util.LocationDistanceUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,18 +17,14 @@ public class LandmarkFindNearService {
     private final LandmarkRepository landmarkRepository;
 
     @Transactional(readOnly = true)
-    public FindNearLandmark.Response findNearLandmark(double latitude, double longitude) {
+    public FindNearLandmarkResponse findNearLandmark(double latitude, double longitude) {
         List<Landmark> landmarks = landmarkRepository.findAll();
 
-        FindNearLandmark.Response ret = new FindNearLandmark.Response();
-        double nearDistance = -1;
+        FindNearLandmarkResponse ret = FindNearLandmarkResponse.createEmptyResponse();
         for (Landmark landmark : landmarks) {
             double distance = LocationDistanceUtils.distance(landmark.getLatitude(), landmark.getLongitude(), latitude, longitude);
             if (distance <= landmark.getRecognitionRadius()) {
-                if (nearDistance == -1 || nearDistance > distance) {
-                    ret = FindNearLandmark.Response.of(landmark, distance);
-                    nearDistance = distance;
-                }
+                ret.update(landmark, distance);
             }
         }
         return ret;

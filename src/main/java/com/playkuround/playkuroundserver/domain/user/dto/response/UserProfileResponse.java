1fd09aef8f4740737325c3b4eef9109ce042a1e0
seungtaekhong <hsk4991149@naver.com>
2023-11-15 17:34:41 +0900

    fix: docs

--- src/main/java/com/playkuround/playkuroundserver/domain/user/dto/response/UserProfileResponse.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/user/dto/response/UserProfileResponse.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.playkuround.playkuroundserver.domain.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -25,8 +26,8 @@ public class UserProfileResponse {
     @Schema(description = "자신의 토탈 스코어 최고점", example = "1500", requiredMode = RequiredMode.REQUIRED)
     private Long highestScore;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    @Schema(description = "최근 출석 날짜. 출석한 적이 없다면 회원가입일 기준 1일 전으로 세팅됨", example = "2023-11-13 13:12:31", requiredMode = RequiredMode.REQUIRED)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Schema(description = "최근 출석 날짜. 출석한 적이 없다면 회원가입일 기준 1일 전으로 세팅됨", example = "2023-11-13 13:12:31", type = "string", requiredMode = RequiredMode.REQUIRED)
     private LocalDateTime lastAttendanceDate;
 
     public static UserProfileResponse from(User user) {

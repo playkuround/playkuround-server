031f6397b35c14434666aab70832c1abd6b56c27
seungtaekhong <hsk4991149@naver.com>
2023-12-18 16:51:41 +0900

    test: 출석하기 api 테스트 추가

---
+++ src/test/java/com/playkuround/playkuroundserver/domain/attendance/api/AttendanceApiTest.java
@@ -0,0 +1,112 @@
+package com.playkuround.playkuroundserver.domain.attendance.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playkuround.playkuroundserver.domain.attendance.application.AttendanceRegisterService;
+import com.playkuround.playkuroundserver.domain.attendance.dao.AttendanceRepository;
+import com.playkuround.playkuroundserver.domain.attendance.dto.request.AttendanceRegisterRequest;
+import com.playkuround.playkuroundserver.domain.badge.dao.BadgeRepository;
+import com.playkuround.playkuroundserver.domain.badge.domain.Badge;
+import com.playkuround.playkuroundserver.domain.badge.domain.BadgeType;
+import com.playkuround.playkuroundserver.domain.user.dao.UserFindDao;
+import com.playkuround.playkuroundserver.domain.user.dao.UserRepository;
+import com.playkuround.playkuroundserver.domain.user.domain.User;
+import com.playkuround.playkuroundserver.securityConfig.WithMockCustomUser;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+class AttendanceApiTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserFindDao userFindDao;
+
+    @Autowired
+    private AttendanceRepository attendanceRepository;
+
+    @Autowired
+    private AttendanceRegisterService attendanceRegisterService;
+
+    @Autowired
+    private BadgeRepository badgeRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @AfterEach
+    void clean() {
+        userRepository.deleteAll();
+        badgeRepository.deleteAll();
+        attendanceRepository.deleteAll();
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("출석 + 최초 출석 배지 받기")
+    void saveAttendance() throws Exception {
+        // given
+        AttendanceRegisterRequest attendanceRegisterRequest = new AttendanceRegisterRequest(37.539927, 127.073006);
+        String request = objectMapper.writeValueAsString(attendanceRegisterRequest);
+
+        // expected
+        mockMvc.perform(post("/api/attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.response.newBadges[?(@.name == '%s')]", "ATTENDANCE_1").exists())
+                .andDo(print());
+
+        List<Badge> badges = badgeRepository.findAll();
+        assertThat(badges.size()).isEqualTo(1);
+        assertThat(badges.get(0).getBadgeType()).isEqualTo(BadgeType.ATTENDANCE_1);
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("중복 출석 요청 - 에러발생")
+    void duplicateAttendance() throws Exception {
+        // given
+        User user = userFindDao.findByEmail("tester@konkuk.ac.kr");
+        AttendanceRegisterRequest attendanceRegisterRequest = new AttendanceRegisterRequest(37.539927, 127.073006);
+        attendanceRegisterService.registerAttendance(user, attendanceRegisterRequest);
+
+        String request = objectMapper.writeValueAsString(attendanceRegisterRequest);
+
+        // expected
+        mockMvc.perform(post("/api/attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.isSuccess").value(false))
+                .andExpect(jsonPath("$.errorResponse.status").value(400))
+                .andExpect(jsonPath("$.errorResponse.code").value("AT01"))
+                .andExpect(jsonPath("$.errorResponse.message").value("이미 오늘 출석한 회원입니다."))
+                .andDo(print());
+    }
+
+}
--- src/test/java/com/playkuround/playkuroundserver/securityConfig/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/playkuround/playkuroundserver/securityConfig/WithMockCustomUserSecurityContextFactory.java
@@ -1,7 +1,9 @@
 package com.playkuround.playkuroundserver.securityConfig;
 
+import com.playkuround.playkuroundserver.domain.user.dao.UserRepository;
 import com.playkuround.playkuroundserver.domain.user.domain.User;
 import com.playkuround.playkuroundserver.global.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,16 +11,22 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+@Component
 public class WithMockCustomUserSecurityContextFactory
         implements WithSecurityContextFactory<WithMockCustomUser> {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
         User user = new User(annotation.email(), annotation.nickname(), annotation.major(), annotation.role());
+        userRepository.save(user);
         String roleName = user.getRole().toString();
         List<String> role = Arrays.stream(roleName.split(",")).toList();
         UserDetailsImpl userDetails = new UserDetailsImpl(user, role, "");

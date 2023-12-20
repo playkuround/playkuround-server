da116d908c46df49967c84cdfb2d2775678ad65d
seungtaekhong <hsk4991149@naver.com>
2023-12-20 13:49:34 +0900

    feat: BadgeAPI Swagger 추가

--- src/main/java/com/playkuround/playkuroundserver/domain/badge/api/BadgeApi.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/badge/api/BadgeApi.java
@@ -5,6 +5,8 @@ import com.playkuround.playkuroundserver.domain.badge.dto.request.BadgeFindReque
 import com.playkuround.playkuroundserver.global.common.response.ApiResponse;
 import com.playkuround.playkuroundserver.global.security.UserDetailsImpl;
 import com.playkuround.playkuroundserver.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +18,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/badges")
 @RequiredArgsConstructor
+@Tag(name = "Badge", description = "Badge API")
 public class BadgeApi {
 
     private final BadgeService badgeService;
 
     @GetMapping
+    @Operation(summary = "뱃지조회", description = "사용자가 획득한 뱃지를 조회합니다.")
     public ApiResponse<List<BadgeFindRequest>> findBadge(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         List<BadgeFindRequest> badges = badgeService.findBadgeByEmail(userDetails.getUser());
         return ApiUtils.success(badges);
--- src/main/java/com/playkuround/playkuroundserver/domain/badge/dto/request/BadgeFindRequest.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/badge/dto/request/BadgeFindRequest.java
@@ -2,6 +2,7 @@ package com.playkuround.playkuroundserver.domain.badge.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.playkuround.playkuroundserver.domain.badge.domain.Badge;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class BadgeFindRequest {
 
+    @Schema(description = "뱃지 이름", example = "ATTENDANCE_7", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
+
+    @Schema(description = "뱃지 설명", example = "7일 연속 출석", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Schema(description = "뱃지를 획득한 날짜, 시각", example = "2023-12-20 11:13:21", type = "string", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createdAt;
 
     public static BadgeFindRequest from(Badge badge) {

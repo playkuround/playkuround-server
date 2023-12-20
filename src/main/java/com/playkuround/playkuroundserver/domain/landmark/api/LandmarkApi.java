f89d67d0fbba1ab1dd803593bab46077206d477f
seungtaekhong <hsk4991149@naver.com>
2023-12-20 14:06:51 +0900

    feat: Landmark API Swagger 추가

--- src/main/java/com/playkuround/playkuroundserver/domain/landmark/api/LandmarkApi.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/landmark/api/LandmarkApi.java
@@ -6,6 +6,8 @@ import com.playkuround.playkuroundserver.global.common.response.ApiResponse;
 import com.playkuround.playkuroundserver.global.util.ApiUtils;
 import com.playkuround.playkuroundserver.global.validation.Latitude;
 import com.playkuround.playkuroundserver.global.validation.Longitude;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/landmarks")
 @RequiredArgsConstructor
+@Tag(name = "Landmark", description = "Landmark API")
 public class LandmarkApi {
 
     private final LandmarkFindNearService landmarkFindNearService;
 
     @GetMapping
+    @Operation(summary = "가장 가까운 랜드마크 찾기",
+            description = "인식 반경 내에 있는 랜드마크 중 가장 가까운 랜드마크를 반환합니다." +
+                    "인식 반경에 랜드마크가 없을 경우 아무것도 반환하지 않습니다.")
     public ApiResponse<FindNearLandmarkResponse> LandmarkFindNear(@RequestParam @Latitude Double latitude,
                                                                   @RequestParam @Longitude Double longitude) {
         FindNearLandmarkResponse nearLandmarkResponse = landmarkFindNearService.findNearestLandmark(latitude, longitude);
--- src/main/java/com/playkuround/playkuroundserver/domain/landmark/dto/response/FindNearLandmarkResponse.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/landmark/dto/response/FindNearLandmarkResponse.java
@@ -2,15 +2,21 @@ package com.playkuround.playkuroundserver.domain.landmark.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.playkuround.playkuroundserver.domain.landmark.domain.Landmark;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
 public class FindNearLandmarkResponse {
 
-    private Long landmarkId;
+    @Schema(description = "랜드마크 이름", example = "동물생명과학관", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
+
+    @Schema(description = "사용자와의 거리(단위:m)", example = "0.0949307305463114", requiredMode = Schema.RequiredMode.REQUIRED)
     private Double distance;
+
+    @Schema(description = "랜드마크 ID", example = "4", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long landmarkId;
 
     private FindNearLandmarkResponse() {
     }

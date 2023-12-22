4d9cedff7523e62679f59173094175d918b6475f
seungtaekhong <hsk4991149@naver.com>
2023-12-22 12:36:56 +0900

    feat: Top100 랭킹조회 API 생성

--- src/main/java/com/playkuround/playkuroundserver/domain/score/api/ScoreApi.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/score/api/ScoreApi.java
@@ -1,29 +1,27 @@
 package com.playkuround.playkuroundserver.domain.score.api;
 
 import com.playkuround.playkuroundserver.domain.score.application.ScoreService;
+import com.playkuround.playkuroundserver.domain.score.dto.response.ScoreRankingResponse;
+import com.playkuround.playkuroundserver.global.common.response.ApiResponse;
+import com.playkuround.playkuroundserver.global.security.UserDetailsImpl;
+import com.playkuround.playkuroundserver.global.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/scores")
 public class ScoreApi {
 
     private final ScoreService scoreService;
-//
-//    @GetMapping("/rankings/top100")
-//    public ApiResponse<List<ScoreRankingDto>> scoreGetTop100() {
-//        List<ScoreRankingDto> response = scoreService.getTop100();
-//        return ApiUtils.success(response);
-//    }
-//
-//    @GetMapping("/rankings")
-//    public ApiResponse<ScoreRankingDto> scoreGetRanking(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        ScoreRankingDto response = scoreService.getMyRank(userDetails.getUser());
-//        return ApiUtils.success(response);
-//    }
+
+    @GetMapping("/rankings/top100")
+    public ApiResponse<ScoreRankingResponse> getScoreTop100(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ScoreRankingResponse response = scoreService.getRankTop100(userDetails.getUser());
+        return ApiUtils.success(response);
+    }
 
 }

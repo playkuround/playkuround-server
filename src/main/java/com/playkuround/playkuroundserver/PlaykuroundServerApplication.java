161dd87a4d164cdb08e0ace62cd390cbcb7a99db
seungtaekhong <hsk4991149@naver.com>
2024-02-12 23:25:42 +0900

    feat: ec2 파일 로깅 테스트

--- src/main/java/com/playkuround/playkuroundserver/PlaykuroundServerApplication.java
+++ src/main/java/com/playkuround/playkuroundserver/PlaykuroundServerApplication.java
@@ -1,6 +1,7 @@
 package com.playkuround.playkuroundserver;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +11,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@Slf4j
 public class PlaykuroundServerApplication {
 
     @PostConstruct
     public static void setTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.debug("log.debug");
+        log.info("log.info");
+        log.warn("log.warn");
+        log.error("log.error");
     }
 
     @Bean

d5e8ae800637a9d3e688f92be49ad77ca1e1582f
seungtaekhong <hsk4991149@naver.com>
2024-02-22 08:43:58 +0900

    fix: PK 데이터 타입을 Long으로 수정

--- src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/AuthVerifyTokenRepository.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/AuthVerifyTokenRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AuthVerifyTokenRepository extends JpaRepository<AuthVerifyToken, String> {
+public interface AuthVerifyTokenRepository extends JpaRepository<AuthVerifyToken, Long> {
 
     boolean existsByAuthVerifyToken(String authVerifyToken);
 
--- src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/RefreshTokenRepository.java
+++ src/main/java/com/playkuround/playkuroundserver/domain/auth/token/dao/RefreshTokenRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     boolean existsByUserEmail(String userEmail);
 

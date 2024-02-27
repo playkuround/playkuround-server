7d9de36bce54861c7824ab7c27e6db5ac321e4cd
seungtaekhong <hsk4991149@naver.com>
2024-02-27 23:40:40 +0900

    test: 뱃지 수동 등록 API test

--- src/test/java/com/playkuround/playkuroundserver/domain/badge/api/BadgeApiTest.java
+++ src/test/java/com/playkuround/playkuroundserver/domain/badge/api/BadgeApiTest.java
@@ -1,9 +1,14 @@
 package com.playkuround.playkuroundserver.domain.badge.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playkuround.playkuroundserver.TestUtil;
 import com.playkuround.playkuroundserver.domain.badge.dao.BadgeRepository;
 import com.playkuround.playkuroundserver.domain.badge.domain.Badge;
 import com.playkuround.playkuroundserver.domain.badge.domain.BadgeType;
+import com.playkuround.playkuroundserver.domain.badge.dto.request.ManualBadgeSaveRequest;
 import com.playkuround.playkuroundserver.domain.user.dao.UserRepository;
+import com.playkuround.playkuroundserver.domain.user.domain.Major;
+import com.playkuround.playkuroundserver.domain.user.domain.Role;
 import com.playkuround.playkuroundserver.domain.user.domain.User;
 import com.playkuround.playkuroundserver.securityConfig.WithMockCustomUser;
 import org.junit.jupiter.api.AfterEach;
@@ -12,9 +17,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
@@ -29,13 +36,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BadgeApiTest {
 
     @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private BadgeRepository badgeRepository;
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @AfterEach
     void clean() {
@@ -104,5 +114,102 @@ class BadgeApiTest {
         List<Badge> badges = badgeRepository.findByUser(userRepository.findAll().get(0));
         assertThat(badges).hasSize(1);
         assertThat(badges.get(0).getBadgeType()).isEqualTo(BadgeType.THE_DREAM_OF_DUCK);
+    }
+
+    @Test
+    @WithMockCustomUser(role = Role.ROLE_ADMIN)
+    @DisplayName("뱃지 수동 등록")
+    void saveManualBadge_1() throws Exception {
+        // given
+        User user = TestUtil.createUser("aa@konkuk.ac.kr", "test", Major.건축학부);
+        userRepository.save(user);
+
+        ManualBadgeSaveRequest manualBadgeSaveRequest
+                = new ManualBadgeSaveRequest(user.getEmail(), BadgeType.MONTHLY_RANKING_1.name(), false);
+        String request = objectMapper.writeValueAsString(manualBadgeSaveRequest);
+
+        // expect
+        mockMvc.perform(post("/api/badges/manual")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.response").value(true))
+                .andDo(print());
+
+        List<Badge> badges = badgeRepository.findByUser(user);
+        assertThat(badges).hasSize(1);
+        assertThat(badges.get(0).getBadgeType()).isEqualTo(BadgeType.MONTHLY_RANKING_1);
+        assertThat(user.getNotification()).isNull();
+    }
+
+    @Test
+    @WithMockCustomUser(role = Role.ROLE_ADMIN)
+    @DisplayName("뱃지 수동 등록 : 개인 메시지 저장")
+    void saveManualBadge_2() throws Exception {
+        // given
+        User user = TestUtil.createUser("aa@konkuk.ac.kr", "test", Major.건축학부);
+        userRepository.save(user);
+
+        ManualBadgeSaveRequest manualBadgeSaveRequest
+                = new ManualBadgeSaveRequest(user.getEmail(), BadgeType.MONTHLY_RANKING_1.name(), true);
+        String request = objectMapper.writeValueAsString(manualBadgeSaveRequest);
+
+        // expect
+        mockMvc.perform(post("/api/badges/manual")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.response").value(true))
+                .andDo(print());
+
+        List<Badge> badges = badgeRepository.findByUser(user);
+        assertThat(badges).hasSize(1);
+        assertThat(badges.get(0).getBadgeType()).isEqualTo(BadgeType.MONTHLY_RANKING_1);
+
+        Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
+        assertThat(optionalUser.get().getNotification()).isEqualTo("new_badge#" + BadgeType.MONTHLY_RANKING_1.name());
+    }
+
+    @Test
+    @WithMockCustomUser(role = Role.ROLE_ADMIN)
+    @DisplayName("뱃지 수동 등록 : 이미 가지고 있는 뱃지면 false를 반환한다.")
+    void saveManualBadge_3() throws Exception {
+        // given
+        User user = TestUtil.createUser("aa@konkuk.ac.kr", "test", Major.건축학부);
+        userRepository.save(user);
+        badgeRepository.save(new Badge(user, BadgeType.MONTHLY_RANKING_1));
+
+        ManualBadgeSaveRequest manualBadgeSaveRequest
+                = new ManualBadgeSaveRequest(user.getEmail(), BadgeType.MONTHLY_RANKING_1.name(), true);
+        String request = objectMapper.writeValueAsString(manualBadgeSaveRequest);
+
+        // expect
+        mockMvc.perform(post("/api/badges/manual")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.response").value(false))
+                .andDo(print());
+
+        List<Badge> badges = badgeRepository.findByUser(user);
+        assertThat(badges).hasSize(1);
+        assertThat(user.getNotification()).isNull();
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("뱃지 수동 등록 : admin 권한이 없으면 403을 반환한다.")
+    void saveManualBadge_4() throws Exception {
+        // given
+        ManualBadgeSaveRequest manualBadgeSaveRequest
+                = new ManualBadgeSaveRequest("test@konkuk.ac.kr", BadgeType.MONTHLY_RANKING_1.name(), true);
+        String request = objectMapper.writeValueAsString(manualBadgeSaveRequest);
+
+        // expect
+        mockMvc.perform(post("/api/badges/manual")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isForbidden())
+                .andDo(print());
     }
 }

16e2932f543ba8fcc1c4716d5f91626a43c3bddb
seungtaekhong <hsk4991149@naver.com>
2024-03-11 12:39:13 +0900

    test: 개인 뱃지 알림(notification) service 테스트

--- src/test/java/com/playkuround/playkuroundserver/domain/user/application/UserProfileServiceTest.java
+++ src/test/java/com/playkuround/playkuroundserver/domain/user/application/UserProfileServiceTest.java
@@ -3,6 +3,7 @@ package com.playkuround.playkuroundserver.domain.user.application;
 import com.playkuround.playkuroundserver.TestUtil;
 import com.playkuround.playkuroundserver.domain.user.dao.UserRepository;
 import com.playkuround.playkuroundserver.domain.user.domain.User;
+import com.playkuround.playkuroundserver.domain.user.dto.response.UserNotificationResponse;
 import com.playkuround.playkuroundserver.domain.user.dto.response.UserProfileResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,6 +15,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -96,4 +99,59 @@ class UserProfileServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("개인 알림 얻기 테스트")
+    class getNotification {
+
+        @Test
+        @DisplayName("알림이 없으면 빈 리스트 리턴")
+        void success_1() {
+            // given
+            User user = TestUtil.createUser();
+
+            // when
+            List<UserNotificationResponse> result = userProfileService.getNotification(user);
+
+            // then
+            assertThat(result).isEmpty();
+        }
+
+        @Test
+        @DisplayName("뱃지 알림이 1개인 경우")
+        void success_2() {
+            // given
+            User user = TestUtil.createUser();
+            user.addNewBadgeNotification("new badge");
+
+            // when
+            List<UserNotificationResponse> result = userProfileService.getNotification(user);
+
+            // then
+            assertThat(result).hasSize(1);
+            assertThat(result.get(0).getName()).isEqualTo("new_badge");
+            assertThat(result.get(0).getDescription()).isEqualTo("new badge");
+            assertThat(user.getNotification()).isNull();
+        }
+
+        @Test
+        @DisplayName("뱃지 알림이 2개 이상인 경우")
+        void success_3() {
+            // given
+            User user = TestUtil.createUser();
+            user.addNewBadgeNotification("new badge1");
+            user.addNewBadgeNotification("new badge2");
+
+            // when
+            List<UserNotificationResponse> result = userProfileService.getNotification(user);
+
+            // then
+            assertThat(result).hasSize(2);
+            assertThat(result.get(0).getName()).isEqualTo("new_badge");
+            assertThat(result.get(0).getDescription()).isEqualTo("new badge1");
+            assertThat(result.get(1).getName()).isEqualTo("new_badge");
+            assertThat(result.get(1).getDescription()).isEqualTo("new badge2");
+            assertThat(user.getNotification()).isNull();
+        }
+
+    }
 }
